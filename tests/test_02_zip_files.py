87545589a7d13ebad2a895ed1a554f45ee991a1f
Hanna <anna.kozyreva.w@gmail.com>
2023-04-06 21:44:32 +0300

    change directories

--- tests/test_02_zip_files.py
+++ tests/test_02_zip_files.py
@@ -2,19 +2,24 @@ import os
 import zipfile
 from os.path import basename
 
-current_dir = os.path.dirname(__file__)
+this_dir = os.path.dirname(__file__)
 path_xlsx = os.path.abspath('Financial Sample.xlsx')
 
-path_files = os.path.join(current_dir, '..', 'resources')
+path_files = os.path.join(os.path.dirname(this_dir), '..', 'resources')
 file_dir = os.listdir(path_files)
 
-with zipfile.ZipFile('..\\resources\\myZip.zip', mode='w', compression=zipfile.ZIP_DEFLATED) as my_zip:
-    for files in file_dir:
-        add_files = os.path.join(path_files, files)
-        my_zip.write(add_files, basename(add_files))
+def test_zip_files(download_files):
+    with zipfile.ZipFile('..\\resources\\myZip.zip', mode='w', compression=zipfile.ZIP_DEFLATED) as my_zip:
+        for files in file_dir:
+            add_files = os.path.join(path_files, files)
+            my_zip.write(add_files, basename(add_files))
 
-with zipfile.ZipFile('..\\resources\\myZip.zip', mode='a', compression=zipfile.ZIP_DEFLATED) as add_zip:
-    add_zip.write(path_xlsx, basename(path_xlsx))
+def test_add_zip_file():
+    with zipfile.ZipFile('..\\resources\\myZip.zip', mode='a', compression=zipfile.ZIP_DEFLATED) as add_zip:
+        add_zip.write(path_xlsx, basename(path_xlsx))
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-create_sours = os.path.abspath(os.path.join(current_dir, '..', 'resources'))
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    create_sours = os.path.abspath(os.path.join(current_dir, '..', 'resources'))
+
+def test1():
+    print(path_files)
